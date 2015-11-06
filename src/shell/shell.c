7fc3cf4210f5a334cc615305da7cff8d464c4bfc
XiQingping <xi.qingping@gmail.com>
2015-11-06 23:05:45 +0800

    shell去除对stdio的依赖

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -20,32 +20,6 @@
 
 static unsigned char is_exit;
 
-char shell_ask_yes_no(const char *prompt) {
-    int c;
-
-    while (1) {
-        if (prompt) {
-            printf("%s\n", prompt);
-        }
-
-        printf("Yes or No? [YN]:");
-        c = fgetc(stdin);
-        fputc(c, stdout);
-
-        if (c == 'y' || c == 'Y') {
-            printf("y\n");
-            return 1;
-        }
-        if (c == 'n' || c == 'N') {
-            printf("n\n");
-            return 0;
-        }
-
-        printf("\nYou must input 'Y' nor 'N'\n");
-    }
-}
-
-
 // 'Not implemented' handler for shell comands
 void shellh_not_implemented_handler(int argc, char **argv) {
     printf(SHELL_ERRMSG);
@@ -142,8 +116,6 @@ static const shell_command_t *shell_execute_command_in_commands(const shell_comm
     const shell_command_t *pcmd;
     for (pcmd = cmds; pcmd->cmd != NULL; ++pcmd) {
         if (!strcasecmp(pcmd->cmd, argv[ 0 ])) {
-            // Special case: the "exit" command has a NULL handler
-            // Special case: "lua" is not allowed in non-interactive mode
             pcmd->handler(argc, argv);
             return pcmd;
         }
@@ -268,6 +240,7 @@ void shell_execute_command(char *cmd) {
     printf("Command \"%s\" cannot be found.\n", argv[0]);
 }
 
+/*
 void getline(char *buf, int buf_size) {
     int i;
     int c;
@@ -295,6 +268,7 @@ void getline(char *buf, int buf_size) {
 
     *buf = 0;
 }
+*/
 
 extern void shell_getline(char *buf, int buf_size);
 
--- src/shell/shell.h
+++ src/shell/shell.h
@@ -11,7 +11,7 @@ void shell_loop(shell_get_line, void *priv_data);
 /// \param prompt 询问时候的提示语.
 ///
 /// \return !=0 输入的是Yes; ==0 输入的是No.
-char shell_ask_yes_no(const char *prompt);
+//char shell_ask_yes_no(const char *prompt);
 
 #endif
 
