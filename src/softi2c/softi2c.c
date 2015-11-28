8e9f944c8055c627436f81a117ae2a862f93cd9d
XiQingping <xi.qingping@gmail.com>
2015-11-28 13:37:25 +0800

    add softi2c libarary

---
+++ src/softi2c/softi2c.c
@@ -0,0 +1,164 @@
+#include "softi2c_platform.h"
+
+/// \name  SoftI2C
+/// @{
+
+/// \brief __start 在总线上产生I2C总线周期开始信号.
+///
+/// \param i2c 底层接口
+static void  inline __start(const struct softi2c_platform *__FAR i2c) {
+    i2c->scl(i2c, 1);
+    i2c->sda(i2c, 1);
+    i2c->sda(i2c, 0);
+    i2c->scl(i2c, 0);
+}
+
+/// \brief __start 在总线上产生I2C总线周期结束信号.
+///
+/// \param i2c 底层接口
+static void  inline __stop(const struct softi2c_platform *__FAR i2c) {
+    i2c->sda(i2c, 0);
+    i2c->scl(i2c, 1);
+    i2c->sda(i2c, 1);
+}
+
+static void inline __restart(const struct softi2c_platform *__FAR i2c) {
+    i2c->sda(i2c, 1);
+    i2c->scl(i2c, 1);
+    i2c->sda(i2c, 0);
+    i2c->scl(i2c, 0);
+}
+
+/// \brief __onebit 向I2C总线产生一个BIT的信号, 并读取这个时钟总线上的数据.
+///
+/// \param i2c 底层接口
+/// \param bit !=0 产生SDA=1的BIT信号; ==0 产生SDA=0的BIT信号.
+///
+/// \return !=0 这个时钟周期总线上读取的SDA=1; ==0; 这个时钟周期总线上读取到SDA=0.
+static char inline __onebit(const struct softi2c_platform *__FAR i2c, char bit) {
+    unsigned char timeout = 0;
+    i2c->sda(i2c, bit);
+    while (i2c->scl(i2c, 1) == 0) {
+        if (timeout++ > 100) {
+            break;
+        }
+    }
+    bit = i2c->sda(i2c, bit);
+    i2c->scl(i2c, 0);
+    return bit;
+}
+
+/// \brief __onebyte 向I2C总线上发送一个BYTE的数据.
+///
+/// \param i2c 底层接口
+/// \param dat 需要发送的数据.
+///
+/// \return 这个字节周期总线上读取到的数据.
+static unsigned char inline __onebyte(const struct softi2c_platform *__FAR i2c, unsigned char dat) {
+    unsigned char ret;
+    unsigned char bit;
+    for (ret = 0, bit = 0x80; bit != 0x00; bit = bit >> 1) {
+        if (__onebit(i2c, dat & bit)) {
+            ret |= bit;
+        }
+    }
+    return ret;
+}
+
+void softi2c_init(const struct softi2c_platform *__FAR i2c) {
+    i2c->init(i2c );
+    i2c->sda(i2c, 1);
+    i2c->scl(i2c, 1);
+}
+
+unsigned char softi2c_write(const struct softi2c_platform *__FAR i2c,
+                            unsigned char addr,
+                            const unsigned char *__FAR dat,
+                            unsigned char len) {
+    unsigned char i;
+
+    __start(i2c); // start
+    __onebyte(i2c, addr << 1); // addr + Write
+    if (0 != __onebit(i2c, 1)) { // check ACK
+        __stop(i2c);
+        return 0;
+    }
+
+    for (i = 0; i < len; ++i) {
+        __onebyte(i2c, *dat++);
+        if (0 != __onebit(i2c, 1)) { // check ACK
+            break;
+        }
+    }
+    __stop(i2c);
+    return i;
+}
+
+unsigned char softi2c_read(const struct softi2c_platform *__FAR i2c,
+                           unsigned char addr,
+                           unsigned char *__FAR dat,
+                           unsigned char len) {
+    unsigned char i;
+
+    __start(i2c); // start
+    __onebyte(i2c, (addr << 1)  + 1); // addr + Read
+    if (0 != __onebit(i2c, 1)) { // check ACK
+        __stop(i2c);
+        return 0;
+    }
+
+    for (i = 0; i < len;) {
+        ++i;
+        *dat++ = __onebyte(i2c, 0xFF);
+        __onebit(i2c, i >= len); // ACK , last NACK
+    }
+    __stop(i2c);
+    return i;
+}
+
+unsigned char softi2c_write_then_read(const struct softi2c_platform *__FAR i2c,
+                                      unsigned char addr,
+                                      const unsigned char *__FAR w,
+                                      unsigned char wlen,
+                                      unsigned char *__FAR r,
+                                      unsigned char rlen) {
+    unsigned char written = 0;
+    unsigned char readed = 0;
+
+    __start(i2c); // start
+    if (w != 0 && wlen != 0) { // write
+        __onebyte(i2c, addr << 1); // addr + Write
+        if (0 != __onebit(i2c, 1)) { // check ACK
+            goto __ret;
+        }
+
+        for (written = 0; written < wlen; ++written) {
+            __onebyte(i2c, *w++);
+            if (0 != __onebit(i2c, 1)) { // check ACK
+                goto __ret;
+            }
+        }
+        if (r != 0 && rlen != 0) { // restart when there some data need to read.
+            __restart(i2c);
+        }
+    }
+
+    if (r != 0 && rlen != 0) {
+        __onebyte(i2c, (addr << 1)  + 1); // addr + Read
+        if (0 != __onebit(i2c, 1)) { // check ACK
+            goto __ret;
+        }
+
+        for (readed = 0; readed < rlen;) {
+            ++readed;
+            *r++ = __onebyte(i2c, 0xFF);
+            __onebit(i2c, readed >= rlen); // ACK , last NACK
+        }
+    }
+
+__ret:
+    __stop(i2c);
+    return readed + written;
+}
+///  @}
+
---
+++ src/softi2c/softi2c.h
@@ -0,0 +1,63 @@
+#ifndef __SOFT_I2C_H__
+#define __SOFT_I2C_H__
+
+#ifndef __FAR
+#define __FAR
+#endif
+
+/// \name  SoftI2C IO模拟软件实现I2C
+/// @{
+
+typedef const struct softi2c_platform *__FAR softi2c_t;
+
+
+/// \brief softi2c_init 初始化.
+///
+/// \param i2c 对应的IO底层接口.
+void softi2c_init(softi2c_t i2c);
+
+/// \brief softi2c_write I2C些数据.
+///
+/// \param i2c 对应的IO底层接口.
+/// \param addr SLAVE地址, 7位, 不包含读写位, 最终总线上的第一个8位周期为:(addr << 1).
+/// \param dat 需要写入的数据.
+/// \param len 需要写入的数据的长度.
+///
+/// \return 写入的数据的长度.
+unsigned char softi2c_write(softi2c_t i2c,
+                            unsigned char addr,
+                            const unsigned char *__FAR dat,
+                            unsigned char len);
+
+/// \brief softi2c_read I2C读取数据.
+///
+/// \param i2c 对应的IO底层接口.
+/// \param addr SLAVE地址, 7位, 不包含读写位, 最终总线上的第一个8位周期为:((addr << 1) + 1).
+/// \param dat 读取的数据存放的地址.
+/// \param len 期望读取的数据长度.
+///
+/// \return 已读取的数据的长度.
+unsigned char softi2c_read(softi2c_t i2c,
+                           unsigned char addr,
+                           unsigned char *__FAR dat,
+                           unsigned char len);
+
+/// \brief softi2c_write_then_read 先写后读.
+///
+/// \param i2c 对应的IO底层接口.
+/// \param addr SLAVE地址, 7位, 不包含读写位, 最终总线上的第一个8位周期为:((addr << 1) + RW).
+/// \param w 需要写入的数据.
+/// \param wlen 需要写入的数据的长度.
+/// \param r 读取的数据存放的地址.
+/// \param rlen 期望读取的数据长度.
+///
+/// \return 写入和读取的数据的字节数相加.
+unsigned char softi2c_write_then_read(softi2c_t i2c,
+                                      unsigned char addr,
+                                      const unsigned char *__FAR w,
+                                      unsigned char wlen,
+                                      unsigned char *__FAR r,
+                                      unsigned char rlen);
+
+///  @}
+#endif
